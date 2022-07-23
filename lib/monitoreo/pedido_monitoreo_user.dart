78492091f0d9d4ae0351e03d8f3d1527f98af586
JoseToledo98 <193761@ids.upchiapas.edu.mx>
2022-07-22 20:29:03 -0500

    fg

--- lib/monitoreo/pedido_monitoreo_user.dart
+++ lib/monitoreo/pedido_monitoreo_user.dart
@@ -2,57 +2,92 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mandaditos_express/layouts/line.dart';
 import 'package:mandaditos_express/monitoreo/monitoreo_controller.dart';
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class pedido_monitoreo_user extends StatefulWidget {
-  const pedido_monitoreo_user ({key, required this.title});
+  const pedido_monitoreo_user({key, required this.title});
   final String title;
 
   @override
   State<pedido_monitoreo_user> createState() => _pedido_monitoreo_userstate();
-
 }
 
 class _pedido_monitoreo_userstate extends State<pedido_monitoreo_user> {
-  
-  final _controller= monitoreo_controller();
+  final api_key = 'AIzaSyBy76EU-NQpNx2NAXxHZH2E-3lj3VNwhW4';
+  final _controller = monitoreo_controller();
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     _controller.addListener(() {
-       setState(() { });
-     });
+      setState(() {});
+    });
   }
- 
+
+  _callNumber() async {
+    const number = '+52 9711491315'; //set the number here
+    bool? res = await FlutterPhoneDirectCaller.callNumber(number);
+  }
 
   @override
   Widget build(BuildContext context) {
-    
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-   return Scaffold(
-    appBar: PreferredSize(preferredSize: const Size.fromHeight(80),
-    child: Container(margin: const EdgeInsets.only(top:40),child: Row(
-                children: <Widget>[Center(child: Container(height: 50,width: 20,margin: const EdgeInsets.only(left: 10),child: Icon(Icons.arrow_back_ios),),)
-                ,Container(height: 70, width: 70,margin: const EdgeInsets.only(left: 10) ,child: Image.asset("assets/images/Logo.png"),)
-                ,]),)),
-   
-    body:  Column( mainAxisAlignment: MainAxisAlignment.start,
-    children: <Widget>[
-       
-           SizedBox(width: width,
-           height: height*.6,
-           child:  GoogleMap(
-            onMapCreated: _controller.onMapCreated,
-            initialCameraPosition: _controller.initialCameraPosition , 
-            zoomGesturesEnabled: false,
-            zoomControlsEnabled: false,
-            markers: _controller.markers,
-            onTap:_controller.onTap ,
-           ))
-    ],
-  )
-   );
+    return Scaffold(
+        appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(80),
+            child: Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: Row(children: <Widget>[
+                Center(
+                  child: Container(
+                    height: 50,
+                    width: 20,
+                    margin: const EdgeInsets.only(left: 10),
+                    child: Icon(Icons.arrow_back_ios),
+                  ),
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: const EdgeInsets.only(left: 10),
+                  child: Image.asset("assets/images/Logo.png"),
+                ),
+              ]),
+            )),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+                width: width,
+                height: height * .6,
+                child: GoogleMap(
+                  onMapCreated: _controller.onMapCreated,
+                  initialCameraPosition: _controller.initialCameraPosition,
+                  zoomGesturesEnabled: false,
+                  zoomControlsEnabled: false,
+                  markers: _controller.markers,
+                  onTap: _controller.onTap,
+                )),
+            Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(left: 100, top: 30),
+                  child: const ElevatedButton(
+                    onPressed: null,
+                    child: Text("Cancelar"),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(left: 100, top: 30),
+                  child: const ElevatedButton(
+                    onPressed: null,
+                    child: Text("Llamar"),
+                  ),
+                )
+              ],
+            )
+          ],
+        ));
   }
-
 }
---
+++ lib/repartidor/googlemapsrepartidor_controller.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+// import 'package:flutter/widgets.dart' show ChangeNotifier;
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+class GoogleMapsRepartidorController extends ChangeNotifier {
+  final Map<MarkerId, Marker> _markers = {};
+  Set<Marker> get markers => _markers.values.toSet();
+
+  // RUTA TRAZADA
+  late PolylinePoints polylinePoints = PolylinePoints();
+  List<LatLng> polylineCoordinates = [];
+
+  final Map<PolylineId, Polyline> _polylines = {};
+  Set<Polyline> get polylines => _polylines.values.toSet();
+
+  createPolylines(double startLatitude, double startLongitude,
+      double destinationLatitude, double destinationLongitude) async {
+    polylinePoints = PolylinePoints();
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+      'AIzaSyBy76EU-NQpNx2NAXxHZH2E-3lj3VNwhW4', // Google Maps API Key
+      PointLatLng(startLatitude, startLongitude),
+      PointLatLng(destinationLatitude, destinationLongitude),
+      travelMode: TravelMode.driving,
+    );
+    log(result.errorMessage.toString());
+    polylineCoordinates.clear();
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    }
+
+    PolylineId polylineId = const PolylineId('poly');
+    final polyline = Polyline(
+      polylineId: polylineId,
+      color: Colors.red,
+      points: polylineCoordinates,
+      width: 3,
+    );
+    // Adding the polyline to the map
+    _polylines[polylineId] = polyline;
+    Set<Polyline>.of(_polylines.values);
+    log(polylines.length.toString());
+    notifyListeners();
+  }
+
+  final initialCameraPosition = const CameraPosition(
+    target: LatLng(16.7432391, -93.1025722),
+    zoom: 15,
+  );
+
+  void onTap(position, name, color, description) {
+    final markerdId = MarkerId(_markers.length.toString());
+    final marker = Marker(
+      markerId: markerdId,
+      position: position,
+      infoWindow: InfoWindow(title: '$name', snippet: '$description'),
+      icon: BitmapDescriptor.defaultMarkerWithHue(color),
+    );
+    _markers[markerdId] = marker;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _markers.clear();
+    super.dispose();
+  }
+}
